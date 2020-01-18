be8e28d41136435b47727a2811c40476e310b5da
filchy <54070973+filchy@users.noreply.github.com>
2020-01-18 16:24:31 +0100

    Update bird_object.py

--- bird_object.py
+++ bird_object.py
@@ -1,78 +1,51 @@
 import pygame
 import os
+import random
 
 
-BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
-			 pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
-			 pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 
-class Bird(object):
-	IMGS = BIRD_IMGS
-	MAX_ROTATION = 25
-	ROT_VEL = 20
-	ANIMATION_TIME = 5
+class Pipe(object):
+	GAP = 200
+	VEL = 5
 
-	def __init__(self, x, y):
+	def __init__(self, x):
 		self.x = x
-		self.y = y
-		self.tilt = 0
-		self.tick_count = 0
-		self.vel = 0
-		self.height = self.y
-		self.img_count = 0
-		self.img = self.IMGS[0]
+		self.height = 0
+		self.gap = 100
 
-	def jump(self):
-		# - moving up
-		# + moving down
-		self.vel = -10.5
-		self.tick_count = 0
-		self.height = self.y
+		self.top = 0
+		self.bottom = 0
+		self.PIPE_BOTTOM = PIPE_IMG
+		self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+
+		self.passed = False
+		self.set_height()
+
+	def set_height(self):
+		self.height = random.randrange(50, 450)
+		self.top = self.height - self.PIPE_TOP.get_height()
+		self.bottom = self.height + self.GAP
 
 	def move(self):
-		self.tick_count += 1
-
-		d = self.vel * self.tick_count + 1.5 * self.tick_count ** 2
-
-		if d >= 16:
-			d = 16
-
-		elif d < 0:
-			d -= 0
-
-		self.y = self.y + d
-
-		# move up
-		if d < 0 or self.y < self.height + 50:
-			if self.tilt < self.MAX_ROTATION:
-				self.tilt = self.MAX_ROTATION
-		# move down
-		else:
-			if self.tilt > -90:
-				self.tilt -= self.ROT_VEL
+		self.x -= self.VEL
 
 	def draw(self, win):
-		self.img_count += 1
+		win.blit(self.PIPE_TOP, (self.x, self.top))
+		win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
 
-		if self.img_count < self.ANIMATION_TIME:
-			self.img = self.IMGS[0]
-		elif self.img_count < self.ANIMATION_TIME*2:
-			self.img = self.IMGS[1]
-		elif self.img_count < self.ANIMATION_TIME*3:
-			self.img = self.IMGS[2]
-		elif self.img_count < self.ANIMATION_TIME*4:
-			self.img = self.IMGS[1]
-		elif self.img_count < self.ANIMATION_TIME*4+1:
-			self.img = self.IMGS[0]
-			self.img_count = 0
+	def collision(self, bird):
+		bird_mask = bird.get_mask()
+		top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+		bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
 
-		if self.tilt <= -80:
-			self.img = self.IMGS[1]
-			self.img_count = self.ANIMATION_TIME*2
+		top_offset = (self.x - bird.x, self.top - round(bird.y))
+		bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
 
-		rotated_image = pygame.transform.rotate(self.img, self.tilt)
-		new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
-		win.blit(rotated_image, new_rect.topleft)
+		b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+		t_point = bird_mask.overlap(top_mask, top_offset)
 
-	def get_mask(self):
-		return pygame.mask.from_surface(self.img)
+		if t_point or b_point:
+			return True
+
+		return False
