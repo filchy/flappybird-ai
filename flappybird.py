b41c05bcb12da8d6162382aaf83a706a02664801
filchy <54070973+filchy@users.noreply.github.com>
2020-01-18 16:25:09 +0100

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -8,14 +8,18 @@ import pygame
 import os
 import random
 import neat
+pygame.font.init()
 
 
 WIN_HEIGHT = 800
 WIN_WIDTH = 500
 
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
-def draw_window(win, birds, pipes, base):
+gen = 0
+
+def draw_window(win, birds, pipes, base, score, gen, pipe_ind):
 	win.blit(BG_IMG, (0,0))
 
 	for pipe in pipes:
@@ -25,10 +29,24 @@ def draw_window(win, birds, pipes, base):
 
 	for bird in birds:
 		bird.draw(win)
+		pygame.draw.line(win, (255,0,0), (bird.x+bird.img.get_width()/2, bird.y + bird.img.get_height()/2),(pipes[pipe_ind].x + pipes[pipe_ind].PIPE_TOP.get_width()/2, pipes[pipe_ind].height), 5)
+		pygame.draw.line(win, (255,0,0), (bird.x+bird.img.get_width()/2, bird.y + bird.img.get_height()/2), (pipes[pipe_ind].x + pipes[pipe_ind].PIPE_BOTTOM.get_width()/2, pipes[pipe_ind].bottom), 5)
+
+	#score
+	score_label = STAT_FONT.render("Score: " + str(score),1,(255,255,255))
+	win.blit(score_label, (WIN_WIDTH - score_label.get_width() - 15, 10))
+
+	#generations
+	score_label = STAT_FONT.render("Gens: " + str(gen-1),1,(255,255,255))
+	win.blit(score_label, (10, 10))
+
+
 	pygame.display.update()
 
 def eval_genomes(genomes, conf):
+	global WIN, gen
 	score = 0
+	gen += 1
 	nets = []
 	ge = []
 	birds = []
@@ -68,6 +86,7 @@ def eval_genomes(genomes, conf):
 			bird.move()
 			ge[x].fitness += 0.1
 
+			# prediction for jump or do not jump -> input: bird y position, bottom pipe y position, upper pipe y position
 			output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
 
 			if output[0] > .5:
@@ -108,11 +127,12 @@ def eval_genomes(genomes, conf):
 				ge.pop(x)
 
 		base.move()
-		draw_window(win, birds, pipes, base)
+		draw_window(win, birds, pipes, base, score, gen, pipe_ind)
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	local_dir = os.path.dirname(__file__)
 	cong_path = os.path.join(local_dir, "config-feedforward.txt")
 	neat_net(cong_path, eval_genomes)
+
